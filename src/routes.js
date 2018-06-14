e7c7a30c5fa38873c3bb2e7fd306f516ff3f0244
z.ovcharov <z.ovcharov@corp.youdo.com>
2018-06-14 20:46:38 +0300

    New boilerplate

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 
 import Home from './components/Home/Home'
-import About from './components/About/About.js';
-import NotFound from './components/NotFound/NotFound.js';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="*" component={NotFound} />
   </Router>
 );
 
